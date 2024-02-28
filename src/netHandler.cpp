514819dccf14d7bd00c3a0ab2fe90912c124944e
Derek Taylor <taylor.3537@osu.edu>
2024-02-28 16:55:58 -0500

    need to add socket integration but can send data to a set IP!

--- src/netHandler.cpp
+++ src/netHandler.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-int main(){
+class NetHandler {
+    
+};
 
+void setConnection(char * addr){
+    struct addrinfo *sinfo;
+    struct addrinfo hints;
+    //sets the bytes of hints to 0
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    //checks if the connection is viable
+    int status = getaddrinfo(addr,"1025", &hints, &sinfo);
+    if(status !=0){
+        std::cout<<gai_strerror(status);
+        exit(1);
+    }
+    else{
+        std::cout<<"made the connection";
+    }
+    //frees the memory reserved in the sinfo struct
+    freeaddrinfo(sinfo);
+}
+int main() {
+    std::string addr = "192.168.0.211";
+    char * charAddr = &addr[0];
+
+    setConnection(charAddr);
     return 0;
 }
